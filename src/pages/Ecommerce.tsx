9b78c0976ab41c32c9e269c9b9ff4436a90c25de
Mujeeb Adebowale <olukayodedayo100@gmail.com>
2022-07-03 19:05:47 +0100

    ecommerce-wip

--- src/pages/Ecommerce.tsx
+++ src/pages/Ecommerce.tsx
@@ -33,13 +33,58 @@ const Ecommerce = () => {
               key={item.title} className="bg-white dart:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl">
               <button type='button' style={{ color: item.iconColor, backgroundColor: item.iconBg }}
               className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl">
-                {item.icon}
-                 
+                {item.icon} 
               </button>
+              <p className="mt-3">
+                <span className="text-lg font-semibold">
+                  {item.amount}
+                </span>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  {item.percentage}
+                </span>
+              </p>
+              <p className='text-sm text-gray-400 mt-1'>{item.title}</p>
 
             </div>
           ))}
         </div>
+      </div>
+      <div className='flex gap-10 flex-wrap justify-center'>
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 rounded-2xl md:w-780">
+          <div className="flex justify-between">
+            <p className="font-semibold text-xl">Revenue Updates</p>
+            <div className='flex items-center gap-4'>
+              <p className='flex items-center gap-2 text-gray-600 '>
+                <span>
+                  <GoPrimitiveDot/> 
+                </span>
+                <span>
+                  Expense
+                </span>
+              </p>
+               <p className='flex items-center gap-2 text-green-600 '>
+                <span>
+                  <GoPrimitiveDot/> 
+                </span>
+                <span>
+                  Budget
+                </span>
+              </p>
+
+            </div>
+          </div>
+          <div className="mt-10 flex gap-10 flex-wrap justify-center">
+            <div className="border-r-1 border-color m-4 pr-10">
+              <div>
+                <p>
+                  <span className='text-3xl font-semibold'>$93,438</span>
+                  <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3'>23%</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   )
