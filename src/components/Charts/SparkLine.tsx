1a9ebc5ec2ff28ac34ba8aae92c02dfb69d18fa0
Mujeeb Adebowale <olukayodedayo100@gmail.com>
2022-07-04 01:54:57 +0100

    Orders-WIP

--- src/components/Charts/SparkLine.tsx
+++ src/components/Charts/SparkLine.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts' 
 
-const SparkLine = () => {
+
+type ChartType = "Line" | "Column" | "WinLoss" | "Pie" | "Area" | undefined
+type SparkLineProps = {
+  id: string,
+  height: string,
+  width: string,
+  color: string,
+  data: { x: number, yval: number }[],
+  type: ChartType,
+  currentColor: string
+}
+
+const SparkLine = ({ id, height, width, color, data, type, currentColor }: SparkLineProps) => {
+   
   return (
-    <div>SparkLine</div>
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName="x"
+      yName='yval'
+      type={type}
+      tooltipSettings={
+        {
+          visible: true,
+          format: '${x} : data ${yval}',
+          trackLineSettings: {
+            visible:true
+          }
+        }  
+      }
+    >
+    <Inject services={[SparklineTooltip]}/>
+    </SparklineComponent>
   )
 }
 
